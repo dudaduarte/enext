a76923511966d6aad123c6430856669c25033851
evilamaior <contatoevilamaior@gmail.com>
2019-07-23 15:48:00 -0300

    creating template string

--- home/home.js
+++ home/home.js
@@ -1,7 +1,25 @@
+const showProducts = document.querySelector('.showProducts');
+
 function getOnApi() {
-    fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
-      .then(response => response.json())
-      .then(response => { const products = response.map(response => response.items);
-      });
-  }
-  getOnApi()
+  fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
+    .then(response => response.json())
+    .then(response => printScreen(response.items))
+}
+
+function printScreen(items) {
+  const products = items[index];
+  showProducts.innerHTML = template(products);
+  index++
+}
+
+function template(products) {
+  return `<div class="card" style="width: 18rem;">
+  <img src="${products.images.imagesUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+          card's content.</p>
+      <a href="#" id="like" class="btn btn-primary">Like</a>
+  </div>
+  </div>`
+}
