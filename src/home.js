d2c4e316d45db5c9284399015c9f4cfabef93a60
evilamaior <contatoevilamaior@gmail.com>
2019-07-23 16:35:06 -0300

    debug show products

--- src/home.js
+++ src/home.js
@@ -3,18 +3,18 @@ const showProducts = document.querySelector('.showProducts');
 function getOnApi() {
   fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
     .then(response => response.json())
-    .then(response => printScreen(response.items))
+    .then(response => printScreen(response))
 }
 
-function printScreen(items) {
-  items.map((item) => {
-    showProducts.innerHTML = template(item);
+function printScreen(response) {
+  response.map((products) => {
+    showProducts.innerHTML = template(products);
   });
 }
 
 function template(products) {
   return `<div class="card" style="width: 18rem;">
-  <img src="${products.images[0].imagesUrl}" class="card-img-top" alt="...">
+  <img src="${products.items[0].images[0].imagesUrl}" class="card-img-top" alt="...">
   <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
