380c4640d93a7865c88b2b44f572babd9b010e0c
evilamaior <contatoevilamaior@gmail.com>
2019-07-23 15:33:56 -0300

    creating template string

--- home.js
+++ home.js
@@ -12,7 +12,14 @@ function printScreen(items) {
   index++
 }
 
-function template (products) {
-  return `
-  `
+function template(products) {
+  return `<div class="card" style="width: 18rem;">
+  <img src="${products.images.imagesUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+          card's content.</p>
+      <a href="#" id="like" class="btn btn-primary">Like</a>
+  </div>
+  </div>`
 }
