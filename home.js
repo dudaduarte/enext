6a30cf85baaf6c0ffc4ff13c74ea89c3183cac41
evilamaior <contatoevilamaior@gmail.com>
2019-07-23 15:22:23 -0300

    getting produts

--- home.js
+++ home.js
@@ -1,7 +1,18 @@
+const showProducts = document.querySelector('.showProducts');
+
 function getOnApi() {
   fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
     .then(response => response.json())
-    .then(response => { const products = response.map(response => response.items);
-    });
+    .then(response => printScreen(response.items))
 }
-getOnApi()
+
+function printScreen(items) {
+  const products = items[index];
+  showProducts.innerHTML = template(products);
+  index++
+}
+
+function template (products) {
+  return `
+  `
+}
