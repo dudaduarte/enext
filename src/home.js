a0f57fdcc00d54b4996740b61dbe63e92f6c692c
evilamaior <contatoevilamaior@gmail.com>
2019-07-24 16:01:49 -0300

    implated like and login

--- src/home.js
+++ src/home.js
@@ -1,31 +1,91 @@
 document.addEventListener("DOMContentLoaded",() => {
-const showProducts = document.querySelector('.showProducts');
-
-function getOnApi() {
-  fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
-    .then(response => response.json())
-    .then(response => printScreen(response))
-}
-
-function printScreen(response) {
-  response.map((products) => {
-    showProducts.innerHTML += template(products);
-  });
-}
-
-function template(products) {
-  return `<div class="card" style="width: 18rem;">
-            <a href="${products.link}">
-              <img src="${products.items[0].images[0].imageUrl}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${products.productName}</h5>
-                <p class="card-text">${products.items[0].sellers[0].commertialOffer.Price}</p>
-                <buton  id="wishlist" class="btn btn-primary" data-id="${products.productId}"><i class="fas fa-heart"></i></button>
-              </div>
-            </a>
-          </div>`
+  const showProducts = document.querySelector('.showProducts');
+  let productsArr = [];
+  let userEmail;
+  getOnApi();
+  
+  async function userListener() {
+    const userData = await fetch('/no-cache/profileSystem/getProfile');
+    const userResponse = await userData.json();
+    if(userResponse.IsUserDefined){
+      userEmail = userResponse.Email;
+      getUserData();
+    } else {
+      vtexid.start({
+        returnUrl: '',
+        userEmail: '',
+        locale: 'pt-BR',
+        forceReload: true
+        });
+    }
   }
-
-getOnApi();
-
+  userListener();
+​
+  function getOnApi() {
+    fetch('https://www.enext.vtexcommercestable.com.br/api/catalog_system/pub/products/search')
+      .then(response => response.json())
+      .then(response => printScreen(response))
+  }
+  
+  function printScreen(response) {
+    response.map((products) => {
+      showProducts.innerHTML += template(products);
+    });
+    const btnLike = document.querySelectorAll('.testebtn');
+    test(btnLike);
+  }
+  
+  function template(products,event) {
+    return `<div class="card mx-5 mb-5 w-25">
+              <a href="${products.link}">
+                <img src="${products.items[0].images[0].imageUrl}" class="card-img-top border-bottom" alt="..." height="329" width="329">
+              </a>
+              <div class="card-body d-flex flex-row">
+                <a href="${products.link}">
+                  <p class="card-title text-dark">${products.productName}</p>
+                </a>
+                <p class="card-text text-dark">RS ${products.items[0].sellers[0].commertialOffer.Price}</p>
+              </div>
+              <div class="ml-auto">
+                <button id="wishlist" class="btn mr-auto" onclick="idLike(${products.productId})" data-id="${products.productId}"><i class="fas fa-heart"></i></button>
+              </div>
+            </div>`
+   }
+​
+  function test(btnLike) {
+    for (btn of btnLike) {
+      btn.addEventListener('click', (event) => {
+        let productId = event.currentTarget.dataset.id;
+        let productIndex = productsArr.indexOf(productId);
+        if (productIndex === -1) {
+          productsArr.push(productId);
+        } else {
+          productsArr.splice(productIndex);
+        }
+        let productsString = productsArr.join(',');
+        patchNewData(productsString)
+      });
+      }
+  }
+​  ​
+  async function getUserData() {
+    const getResponse = await fetch(`https://www.enext.vtexcommercestable.com.br//api/dataentities/CL/search?email=${userEmail}&_fields=wishlistProducts`);
+    const getResponseJson = await getResponse.json();
+    productsArr = getResponseJson[0].wishlistProducts.split(',');
+  }
+  
+  async function patchNewData(productsString) {
+    const patchResponse = await fetch("https://enext.vtexcommercestable.com.br/api/dataentities/CL/documents", {
+      body: JSON.stringify({
+        "email": userEmail,
+        "wishlistProducts": productsString
+      }),
+      headers: {
+        Accept: "application/vnd.vtex.ds.v10+json",
+        "Content-Type": "application/json"
+      },
+      method: "PATCH",
+    });
+    const patchResponseJson = await patchResponse.json();
+  }
 })
